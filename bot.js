8a27b27edba330b5b58860940576066d60af5daf
LiamTur <turelid@chalmers.se>
2024-09-15 18:07:28 +0200

    removed events and put all control in bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.cooldowns = new Collection();
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -23,18 +24,52 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.cooldowns = new Collection();
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 
-const eventsPath = path.join(__dirname, 'events');
-const eventsFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = client.commands.get(interaction.commandName);
 
-for (const file of eventsFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
 	}
-}
+
+	const { cooldowns } = interaction.client;
+
+	if (!cooldowns.has(command.data.name)) {
+		cooldowns.set(command.data.name, new Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.data.name);
+	const defaultCooldownDuration = 3;
+	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+	if (timestamps.has(interaction.user.id)) {
+		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const expiredTimestamp = Math.round(expirationTime / 1000);
+			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+		}
+	}
+
+	timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 client.login(token);
--- commands/utility/randomgen.js
+++ commands/utility/randomgen.js
@@ -3,24 +3,27 @@ const { SlashCommandBuilder } = require('discord.js');
 let inputs = [];
 
 module.exports = {
-    data: [
-        new SlashCommandBuilder()
-            .setName('addinput')
-            .setDescription('Adds an input to the list.')
-            .addStringOption(option =>
-                option.setName('input')
-                    .setDescription('The input to add.')
-                    .setRequired(true)),
-        new SlashCommandBuilder()
-            .setName('chooseinput')
-            .setDescription('Chooses a random input from the list.')
-    ],
+    data: new SlashCommandBuilder()
+        .setName('randomgen')
+        .setDescription('Random generation commands')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("add")
+                .setDescription('Adds an input to the list.')
+                .addStringOption(option =>
+                    option.setName('input')
+                        .setDescription('The input to add.')
+                        .setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("chooseinput")
+                .setDescription('Chooses a random input from the list.')),
     async execute(interaction) {
-        if (interaction.commandName === 'addinput') {
+        if (interaction.options.getSubcommand() === 'addinput') {
             const input = interaction.options.getString('input', true);
             inputs.push(input);
             await interaction.reply(`Input \`${input}\` added!`);
-        } else if (interaction.commandName === 'chooseinput') {
+        } else if (interaction.options.getSubcommand() === 'chooseinput') {
             if (inputs.length === 0) {
                 await interaction.reply('No inputs available to choose from.');
             } else {
